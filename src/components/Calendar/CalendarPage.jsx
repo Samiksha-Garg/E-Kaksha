b92721aaddfba2d7ac5caf7d10ad6bf0e7e510e6
Samiksha Garg <gargsamiksha0306@gmail.com>
2022-03-07 12:48:25 +0530

    added topNavBar

--- src/components/Calendar/CalendarPage.jsx
+++ src/components/Calendar/CalendarPage.jsx
@@ -3,12 +3,16 @@ import { render } from "react-dom";
 import Calendar from "./Calendar";
 import "./index.css";
 import styles from "../../styles/Calendar.module.css"
+import TopNavbar from "../Navigation/topNavbar";
 
 export default () => (
+  <div style={{width : "100%"}}>
+  <TopNavbar/>
   <div className={styles.container}>
     <div className={styles.filters} style={{width : "30%"}}> 
       <h1> Filters</h1>
      </div>
     <Calendar />
+  </div>
   </div>);
 
