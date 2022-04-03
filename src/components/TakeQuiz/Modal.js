7f102d68fdb344991609b8ce839b9f6e6f8405bc
Samiksha Garg <gargsamiksha0306@gmail.com>
2022-04-04 00:05:11 +0530

    viewing correct ans

--- src/components/TakeQuiz/Modal.js
+++ src/components/TakeQuiz/Modal.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React,  {useState , useEffect , useContext} from 'react';
 
-const Modal = ({ onClose, onOpen , results, data }) => {
+const Modal = ({ onClose, onOpen , results, data}) => {
+
   return(
     <div className="modal is-active">
       <div className="modal-background" onClick={onClose}></div>
@@ -12,10 +13,11 @@ const Modal = ({ onClose, onOpen , results, data }) => {
         <section className="modal-card-body content">
           <ul>
             {results.map((result, i) => (
+              
               <li key={i} className="mb-6">
                 <p><strong>{result.q}</strong></p>
-                <p className={result.a === data[i].correct_ans ? 'has-background-success has-text-white p-2' : 'has-background-danger has-text-white p-2'}>Your answer: {result.a}</p>
-                {result.a !== data[i].correct_ans && <p className="has-background-link has-text-white p-2">Correct answer: {data[i].answer}</p>}
+                <p className={result.a === data[i].correctOption ? 'has-background-success has-text-white p-2' : 'has-background-danger has-text-white p-2'}>Your answer: {result.a == '1' ? data[i].option1 : result.a == '2' ? data[i].option2 : result.a == '3' ? data[i].option3 : result.a == '4' ? data[i].option4 : '' }</p>
+                {result.a !== data[i].correctOption && <p className="has-background-link has-text-white p-2">Correct answer: {data[i].correctOption == '1' ? data[i].option1 : data[i].correctOption == '2' ? data[i].option2 : data[i].correctOption == '3' ? data[i].option3 : data[i].correctOption == '4' ? data[i].option4 : ''}</p>}
               </li>
             ))}
           </ul>
