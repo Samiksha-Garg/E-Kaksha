de5cf76afdcf5257db8b4e5a217bda818b9569ae
Kajal <kajalkaushal700@gmail.com>
2022-04-05 01:57:00 +0530

    Finally teacher side attendance done- harsh one to debug :')

--- src/components/Attendance/DialogBox.js
+++ src/components/Attendance/DialogBox.js
@@ -3,8 +3,7 @@ import { useState} from "react";
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
 import {
-  Dialog,
-  FormControlLabel,
+  Dialog
 } from "@mui/material";
 import Button from "@mui/material/Button";
 import ListItemText from "@mui/material/ListItemText";
@@ -14,46 +13,46 @@ import Divider from "@mui/material/Divider";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import { Checkbox } from '@mui/material';
+
 
 
 function DialogBox({courseId,classId,studentName,onSetPresent,present,onSetStep}){
     // console.log(studentName);
-    // let presentStu =[];
+     let presentStu =[];
      const [subModal, setSubModal] = useState(false);
     
-    // const changeHandler1 = (e)=>{
-    //     // if(e.target.checked && !presentStu.includes(e.target.value)){
-    //     //     presentStu.push(e.target.value);
-    //     //     for(let i=0 ; i<studentName.length ; i++){
-    //     //       console.log(e.target.value);
-    //     //       console.log(studentName[i].Attended);
-    //     //       if(e.target.value==studentName[i].Id){
-    //     //         studentName[i].Attended=true;
-    //     //         console.log(i);
-    //     //         break;
-    //     //       }
-    //     //     }
-    //     // }
-    //     // if(!(e.target.checked) && presentStu.includes(e.target.value)){
-    //     //     presentStu = presentStu.filter((ele) =>{return ele != e.target.value;})
-    //     //     for(let i=0 ; i<studentName.length ; i++){
-    //     //       console.log(e.target.value);
-    //     //       console.log(studentName[i].Attended);
-    //     //       if(e.target.value==studentName[i].Id){
-    //     //         studentName[i].Attended=false;
-    //     //         console.log(i);
-    //     //         break;
-    //     //       }
-    //     //     }
-    //     // }
+    const changeHandler1 = (e)=>{
+        if(e.target.checked && !presentStu.includes(e.target.value)){
+            presentStu.push(e.target.value);
+            for(let i=0 ; i<studentName.length ; i++){
+              console.log(e.target.value);
+              console.log(studentName[i].Attended);
+              if(e.target.value==studentName[i].Id){
+                studentName[i].Attended=true;
+                console.log(i);
+                break;
+              }
+            }
+        }
+        if(!(e.target.checked) && presentStu.includes(e.target.value)){
+            presentStu = presentStu.filter((ele) =>{return ele != e.target.value;})
+            for(let i=0 ; i<studentName.length ; i++){
+              console.log(e.target.value);
+              console.log(studentName[i].Attended);
+              if(e.target.value==studentName[i].Id){
+                studentName[i].Attended=false;
+                console.log(i);
+                break;
+              }
+            }
+        }
         
-    // }
+    }
         
-    // const handleClose = () => {
-    //   onSetStep(1);
-    //     onSetPresent(presentStu);
-    //   };
+    const handleClose = () => {
+      onSetStep(1);
+        onSetPresent(presentStu);
+      };
     
 
     return (
@@ -85,9 +84,9 @@ function DialogBox({courseId,classId,studentName,onSetPresent,present,onSetStep}
               <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
                 Assignment: Hello
               </Typography>
-              {/* <Button autoFocus color="inherit" onClick={handleClose}>
+              <Button autoFocus color="inherit" onClick={handleClose}>
               save
-            </Button> */}
+            </Button>
             </Toolbar>
           </AppBar>
           <List>
@@ -98,7 +97,8 @@ function DialogBox({courseId,classId,studentName,onSetPresent,present,onSetStep}
                     <ListItemText
                       primary={event.Name}
                     />
-                   {/* <FormControlLabel  control={<Checkbox defaultChecked={event.Attended} onChange={changeHandler1} ></Checkbox>} value={event.Id} ></FormControlLabel> */}
+                  
+                  <input type="checkbox"  value={event.Id} onChange={changeHandler1}  defaultChecked={event.Attended}></input>
                   </ListItem>
                   <Divider />
                 </div>
