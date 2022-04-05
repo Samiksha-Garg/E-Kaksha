9b1c99f402e592cd21ddf57f5225c18694e9d0d7
Kajal <kajalkaushal700@gmail.com>
2022-04-05 12:15:35 +0530

    removed un-used imports in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import React, {useState, useRef, useContext, useEffect} from "react";
+import React, { useState, useRef, useContext, useEffect } from "react";
 import { Context } from "./context/Context";
 
 //Different components imported to create routes
@@ -9,43 +9,46 @@ import NavBar from "./components/Navigation/NavBar";
 import CalendarPage from "./components/Calendar/CalendarPage";
 import ProtectedRoute from "./ProtectedRoute";
 import Chat from "./components/Chat/Chat";
-import Attendance from "./components/Attendance/AttendanceDecider"
-
-
-import ToDoAsnPage from "./components/ToDoAsn/ToDoAsnPage"
-import AddQuestions from "./pages/Quiz/addQuestions";
-import QuizPage from "./pages/Quiz/QuizPage";
-import AttendancePage from "./components/Attendance/AttendancePage"
-import Display from "./components/TakeQuiz/Dispaly"
+import Attendance from "./components/Attendance/AttendanceDecider";
+import ToDoAsnPage from "./components/ToDoAsn/ToDoAsnPage";
+import Display from "./components/TakeQuiz/Dispaly";
 
 // import "./App.css";
 
-function App(){
-  const {user}=useContext(Context);
-  console.log("app")
-    return (
-
-      <Router>
-      <div style={{display : "flex", width : "100%", minHeight:"100vh"}}>
-      {user? <NavBar/> : null} 
+function App() {
+  const { user } = useContext(Context);
+  console.log("app");
+  return (
+    <Router>
+      <div style={{ display: "flex", width: "100%", minHeight: "100vh" }}>
+        {user ? <NavBar /> : null}
         <Routes>
-        <Route exact path={"/"} element={<HomePage />} />  
-        <Route path={"/calendar"} element={<ProtectedRoute Component={CalendarPage}/>}/>
-        <Route path={"/chat"} element={<ProtectedRoute Component={Chat}/>}/>
-        <Route path={"/assignments"} element={<ProtectedRoute Component={ToDoAsnPage}/>}/>
-        <Route path="/coursepage/:cid" element={<ProtectedRoute Component={CoursePage}/>}/>
-
-        <Route path={"/attendance"} element={<ProtectedRoute Component={Attendance}/>}/>
-
-        
-        <Route path={"/quiz"} element={<ProtectedRoute Component={Display}/>}/>
-
-       </Routes>
-       </div>
-      </Router>
-
-    );
+          <Route exact path={"/"} element={<HomePage />} />
+          <Route
+            path={"/calendar"}
+            element={<ProtectedRoute Component={CalendarPage} />}
+          />
+          <Route path={"/chat"} element={<ProtectedRoute Component={Chat} />} />
+          <Route
+            path={"/assignments"}
+            element={<ProtectedRoute Component={ToDoAsnPage} />}
+          />
+          <Route
+            path="/coursepage/:cid"
+            element={<ProtectedRoute Component={CoursePage} />}
+          />
+          <Route
+            path={"/attendance"}
+            element={<ProtectedRoute Component={Attendance} />}
+          />
+          <Route
+            path={"/quiz"}
+            element={<ProtectedRoute Component={Display} />}
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
-
